9e91ddce32d88a8f3895d6776d730a1cdd556aea
maks bezrodnyi <maks.bezrodnyi@gmail.com>
2019-06-05 23:17:26 +0300

    init commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYmV6cm9kbnlpIiwiYSI6ImNqd2ljdWdqZTA3dTg0OXFrNTJ1cnNwN20ifQ.Wgre1M-_AdUyg78K4grf7w&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
